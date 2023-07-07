00bfc89f0c9081e8cbe23af4303edef43e76abb1
BoyanK95 <boyan_k_95@abv.bg>
2023-07-08 02:49:13 +0300

    feat: create FooterColumn component to hold footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,38 @@
+import { footerLinks } from '@/constants';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
+
+type ColumnProps = {
+    title: string;
+    links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+    <div className='footer_column'>
+        <h4 className='font-semibold'>{title}</h4>
+        <ul className='flex flex-col gap-2 font-normal'>
+            {links.map((link) => (
+                <Link href='/' key={link}>
+                    {link}
+                </Link>
+            ))}
+        </ul>
+    </div>
+);
 
 const Footer = () => {
     return (
         <footer className='flexStart footer'>
             <div className='flex flex-col gap-12 w-full'>
                 <div className='flex items-start flex-col'>
-                    <Image src='/logo-purple.svg' width={115} height={38} alt='Flexible'/>
+                    <Image src='/logo-purple.svg' width={115} height={38} alt='Flexible' />
                     <p className='text-start text-sm font-normal mt-5 max-w-xs'>
                         Flexibble is aming to shine with its ability to create, share, grow your developers projects!
                     </p>
+                </div>
+                <div className='flex flex-wrap gap-12'>
+                    <FooterColumn title={footerLinks[0].title} links={footerLinks[0].links} />
                 </div>
             </div>
         </footer>
