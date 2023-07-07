3698bcb8b32e7e171dbf924ed92e3254293e6f37
BoyanK95 <boyan_k_95@abv.bg>
2023-07-08 01:20:53 +0300

    feat: create initial AuthProvider add Links to Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,28 @@
-import Image from "next/image";
-import Link from "next/link";
+import { NavLinks } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import AuthProviders from './AuthProviders';
 
 const Navbar = () => {
+    const session = {}
     return (
-        <nav className="flexBetween navbar">
-            <div className="flex-1 flexStart gap-10">
+        <nav className='flexBetween navbar'>
+            <div className='flex-1 flexStart gap-10'>
                 <Link href='/'>
-                    <Image src="/logo.svg" width={115} height={43} alt="Flexible image"></Image>
+                    <Image src='/logo.svg' width={115} height={43} alt='Flexible image'></Image>
                 </Link>
-                <ul className="xl:flex hidden text-small gap-7">
-                    
+                <ul className='xl:flex hidden text-small gap-7'>
+                    {NavLinks.map((link, index) => (
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>
+                    ))}
                 </ul>
-            </div>;
+            </div>
+
+            <div className='flexCenter gap-4'>
+                        {session ? <><Link href='/create-project'>Share Work</Link></> : <AuthProviders />}
+            </div>
         </nav>
     );
 };
