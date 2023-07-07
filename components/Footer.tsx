9cb933088605e3a52da2106ab606dcf33a70e929
BoyanK95 <boyan_k_95@abv.bg>
2023-07-08 02:37:16 +0300

    feat: create containers and add flexibble image

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,19 @@
+import Image from 'next/image';
 import React from 'react';
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className='flexStart footer'>
+            <div className='flex flex-col gap-12 w-full'>
+                <div className='flex items-start flex-col'>
+                    <Image src='/logo-purple.svg' width={115} height={38} alt='Flexible'/>
+                    <p className='text-start text-sm font-normal mt-5 max-w-xs'>
+                        Flexibble is aming to shine with its ability to create, share, grow your developers projects!
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
