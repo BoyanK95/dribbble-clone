89eaf709efc4ed676cf1388a48fea9ba1c734d47
BoyanK95 <boyan_k_95@abv.bg>
2023-07-08 01:03:48 +0300

    feat: add link and image to Navbar component, create index.ts content holder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Navbar = () => {
-    return <div>Footer</div>;
+    return (
+        <nav className="flexBetween navbar">
+            <div className="flex-1 flexStart gap-10">
+                <Link href='/'>
+                    <Image src="/logo.svg" width={115} height={43} alt="Flexible image"></Image>
+                </Link>
+                <ul className="xl:flex hidden text-small gap-7">
+                    
+                </ul>
+            </div>;
+        </nav>
+    );
 };
 
 export default Navbar;
