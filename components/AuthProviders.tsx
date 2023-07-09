2e936632e3c1e14da9bb839ce71c306b4f25299b
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-07-09 16:46:55 +0300

    feat: create AuthProviders component (#3)

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+'use client';
+import { useState, useEffect } from 'react';
+import { getProviders, signIn } from 'next-auth/react';
+
+type Provider = {
+    id: string;
+    name: string;
+    type: string;
+    signinUrl: string;
+    callbackUrl: string;
+    signinUrlParams: Record<string, string> | null;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return (
-    <div>AuthProviders</div>
-  )
-}
+    const [providers, setpPoviders] = useState<Providers | null>(null);
 
-export default AuthProviders
+    if (providers) {
+        return (
+            <div>
+                {Object.values(providers).map((provider: Provider, index) => (
+                    <button key={index}>{provider.id}</button>
+                ))}
+            </div>
+        );
+    }
+};
+
+export default AuthProviders;
