584d9094e2252e624130d37cb7bdb7057db563dc
BoyanK95 <boyan_k_95@abv.bg>
2023-07-10 15:55:11 +0300

    feat: create GOOGLE_ID and GOOGLE_SECRET, and add initial logic for callbacks to set up nextAuth

--- lib/session.ts
+++ lib/session.ts
@@ -8,28 +8,36 @@ import { JWT } from 'next-auth/jwt';
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: ''
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
     ],
-    jwt: {
-        encode: ({secret, token}) => {
+    // jwt: {
+    //     encode: ({secret, token}) => {
 
-        },
-        decode: async ({secret, token}) => {
+    //     },
+    //     decode: async ({secret, token}) => {
 
-        }
-    },
+    //     }
+    // },
     theme: {
         colorScheme: 'light',
         logo: '/logo.png'
     },
-    // callbacks: {
-    //     async session({session}) => {
-            
-    //     },
-    //     async signIn({user}) => {
+    callbacks: {
+        async session({ session }) {
+            return session;
+        },
+        async signIn({ user }: { user: AdapterUser | User }) {
+            try {
+                //get the user if they exist
 
-    //     }
-    // }
+                //if they don't exist, create them
+                return true;
+            } catch (error: any) {
+                console.log(error);
+                return false;
+            }
+        }
+    }
 };
