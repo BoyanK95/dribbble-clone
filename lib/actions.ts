cd30ce384cab5365c42bb35302eb80b2cc5a6ddf
BoyanK95 <boyan_k_95@abv.bg>
2023-07-10 18:46:37 +0300

    feat: create actions and makeGraphQLRequest logic

---
+++ lib/actions.ts
@@ -0,0 +1,17 @@
+import { GraphQLClient } from 'graphql-request';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
+const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
+
+const client = new GraphQLClient(apiUrl);
+
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+    try {
+        const client = new GraphQLClient('apiUrl');
+        return await client.request(query, variables);
+    } catch (error) {
+        throw error;
+    }
+};
