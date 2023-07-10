7db50a654c2229109437337f9678b8df0ce6abe2
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-07-10 17:51:24 +0300

    feat: create session logic (#6)
    * feat: create initial authOptions  and handler to handle GET and POST requests with NextAuth
    
    * feat: create GOOGLE_ID and GOOGLE_SECRET, and add initial logic for callbacks to set up nextAuth
    
    * feat: create onClick function for signIn
    
    * feat: created getCurrentUser function and use it in Navbar
    
    * feat:if user - create user image component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,11 @@ import { NavLinks } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 import AuthProviders from './AuthProviders';
+import { getCurrentUser } from '@/lib/session';
 
-const Navbar = () => {
-    const session = null
-    
+const Navbar = async () => {
+    const session = await getCurrentUser();
+
     return (
         <nav className='flexBetween navbar'>
             <div className='flex-1 flexStart gap-10'>
@@ -22,7 +23,22 @@ const Navbar = () => {
             </div>
 
             <div className='flexCenter gap-4'>
-                        {session ? <><Link href='/create-project'>Share Work</Link></> : <AuthProviders />}
+                {session?.user ? (
+                    <>
+                        {session?.user?.image && (
+                            <Image
+                                src={session.user.image}
+                                width={40}
+                                height={40}
+                                className='rounded-full'
+                                alt={session.user.name}
+                            />
+                        )}
+                        <Link href='/create-project'>Share Work</Link>
+                    </>
+                ) : (
+                    <AuthProviders />
+                )}
             </div>
         </nav>
     );
