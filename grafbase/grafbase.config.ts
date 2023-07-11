cc9909411db1d98aa1e45cbc7b2e49a2025ec225
BoyanK95 <boyan_k_95@abv.bg>
2023-07-11 16:19:03 +0300

    feat: add config shema and rules for User and Project models

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,5 +1,7 @@
 import { g, auth, config } from '@grafbase/sdk'
 
+
+//@ts-ignore
 const User = g.model('User', {
   name: g.string().length({min: 2, max: 20}),
   email: g.string().unique(),
@@ -8,8 +10,11 @@ const User = g.model('User', {
   githubUrl: g.url().optional(),
   linkedInUrl: g.url().optional(),
   projects: g.relation(() => Project).list().optional(),
+}).auth((rules) => {
+  rules.public().read()
 })
 
+//@ts-ignore
 const Project = g.model('Project', {
   title: g.string().length({min: 2, max: 20}),
   description: g.string(),
@@ -18,6 +23,9 @@ const Project = g.model('Project', {
   githubUrl: g.url(),
   category: g.string().search(),
   createdBy: g.relation(() => User)
+}).auth((rules) => {
+  rules.public().read(),
+  rules.private().create().delete().update()
 })
 
 const jwt = auth.JWT({
@@ -26,5 +34,9 @@ const jwt = auth.JWT({
 })
 
 export default config({
-  schema: g
+  schema: g,
+  auth: {
+    providers: [jwt],
+    rules: (rules) => rules.private()
+  }
 })
