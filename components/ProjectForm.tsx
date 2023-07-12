d3959702206164dd4bbd40d8816d916aec5c914c
BoyanK95 <boyan_k_95@abv.bg>
2023-07-12 18:33:29 +0300

    feat: create form input and image in ProjectForm

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+'use client';
+import { SessionInterface } from '@/common.type';
+import Image from 'next/image';
+import { ChangeEvent } from 'react';
 
-const ProjectForm = () => {
-  return (
-    <div>ProjectForm</div>
-  )
-}
+type Props = {
+    type: string;
+    session: SessionInterface;
+};
 
-export default ProjectForm
+const handleFormSubmit = (e: React.FormEvent) => {};
+const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {};
+
+const ProjectForm = ({ type, session }: Props) => {
+    const form = {
+        image: ''
+    };
+
+    return (
+        <form onSubmit={handleFormSubmit} className='flexStart form'>
+            <div className='flexStart form_image-label'>
+                <label htmlFor='poster'>{!form.image && 'Chose an image for your project'}</label>
+                <input
+                    id='image'
+                    type='file'
+                    accept='image/*'
+                    required={type === 'create'}
+                    className='form_image-input'
+                    onChange={handleChangeImage}
+                />
+                {form.image && (
+                    <Image src={form.image} className='sm:p-10 object-contain z-20' alt='Project poster' fill />
+                )}
+            </div>
+        </form>
+    );
+};
+
+export default ProjectForm;
