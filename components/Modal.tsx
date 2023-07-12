40de8c023bd2779c0df9f0a05e350b6dc6183dcd
BoyanK95 <boyan_k_95@abv.bg>
2023-07-12 16:34:49 +0300

    feat: create close modal functionallity

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { ReactNode, useRef } from 'react';
+import { ReactNode, useCallback, useRef } from 'react';
 import Image from 'next/image';
 
 const Modal = ({ children }: { children: ReactNode }) => {
@@ -9,15 +9,27 @@ const Modal = ({ children }: { children: ReactNode }) => {
     const wrapper = useRef<HTMLDivElement>(null);
     const router = useRouter();
 
-    const handleClick = () => {};
-    const onDissmiss = () => {};
+    const onDissmiss = useCallback(() => {
+        router.push('/');
+    }, [router]);
+
+    const handleClick = useCallback(
+        (e: React.MouseEvent) => {
+            if (e.target === overlay.current && onDissmiss) {
+                onDissmiss()
+            }
+        },
+        [onDissmiss, overlay]
+    );
 
     return (
         <div ref={overlay} className='modal' onClick={handleClick}>
             <button type='button' onClick={onDissmiss} className='absolute top-4 right-8'>
-                <Image src='/close.svg' width={17} height={17} alt='close'/>
+                <Image src='/close.svg' width={17} height={17} alt='close' />
             </button>
-            <div ref={wrapper} className='modal_wrapper'>{children}</div>
+            <div ref={wrapper} className='modal_wrapper'>
+                {children}
+            </div>
         </div>
     );
 };
