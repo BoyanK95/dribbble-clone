0255780f6aa9620c6c65627d0d42ed1613173138
BoyanK95 <boyan_k_95@abv.bg>
2023-07-12 17:53:53 +0300

    feat: add session and type prop to ProjectForm component

--- app/create-project/page.tsx
+++ app/create-project/page.tsx
@@ -1,11 +1,17 @@
-import Modal from "@/components/Modal";
-import ProjectForm from "@/components/ProjectForm";
+import Modal from '@/components/Modal';
+import ProjectForm from '@/components/ProjectForm';
+import { getCurrentUser } from '@/lib/session';
+import { redirect } from 'next/navigation';
 
-const CreateProject = () => {
+const CreateProject = async () => {
+    const session = await getCurrentUser();
+
+    if (!session.user) redirect('/');
+
     return (
         <Modal>
             <h3 className='modal-head-text'>Create a New Project</h3>
-            <ProjectForm/>
+            <ProjectForm type='create' session={session}/>
         </Modal>
     );
 };
