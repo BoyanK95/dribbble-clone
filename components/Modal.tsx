022848fd9fa8eaf354a788470b8a2aabf01f20e8
BoyanK95 <boyan_k_95@abv.bg>
2023-07-12 16:28:52 +0300

    feat: create initial overlay div and  close button

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+'use client';
 
-const Modal = () => {
-  return (
-    <div></div>
-  )
-}
+import { useRouter } from 'next/navigation';
+import { ReactNode, useRef } from 'react';
+import Image from 'next/image';
 
-export default Modal
+const Modal = ({ children }: { children: ReactNode }) => {
+    const overlay = useRef<HTMLDivElement>(null);
+    const wrapper = useRef<HTMLDivElement>(null);
+    const router = useRouter();
+
+    const handleClick = () => {};
+    const onDissmiss = () => {};
+
+    return (
+        <div ref={overlay} className='modal' onClick={handleClick}>
+            <button type='button' onClick={onDissmiss} className='absolute top-4 right-8'>
+                <Image src='/close.svg' width={17} height={17} alt='close'/>
+            </button>
+            <div ref={wrapper} className='modal_wrapper'>{children}</div>
+        </div>
+    );
+};
+
+export default Modal;
