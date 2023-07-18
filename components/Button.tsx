feb7aee34760048d2effe78dba4a45bffde88e91
BoyanK95 <boyan_k_95@abv.bg>
2023-07-18 17:10:09 +0300

    fix: fix button classes and imageIcon

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,23 +3,30 @@ import { MouseEventHandler } from 'react';
 
 type Props = {
     title: string;
-    leftIcon?: string;
-    rightIcon?: string;
+    leftIcon?: string | null;
+    rightIcon?: string | null;
     handleClick?: MouseEventHandler;
-    isSubmiting?: boolean;
-    type?: 'submit' | 'button';
+    isSubmiting?: boolean | false;
+    type?: 'button' | 'submit';
     bgColor?: string;
     textColor?: string;
 };
 
-const Button = ({ title, leftIcon, rightIcon, handleClick, isSubmiting, type, bgColor, textColor }: Props) => {
-    return (
-        <button className='flexStart gap-3 py-3 px-4' type={type || 'button'} onClick={handleClick} disabled={isSubmiting}>
-            {leftIcon && <Image src={leftIcon} width={14} height={14} alt='left' />}
-            {title}
-            {rightIcon && <Image src={rightIcon} width={14} height={14} alt='right' />}
-        </button>
-    );
-};
+const Button = ({ title, leftIcon, rightIcon, handleClick, isSubmiting, type, bgColor, textColor }: Props) => (
+    <button
+        type={type || 'button'}
+        disabled={isSubmiting || false}
+        className={`flexCenter gap-3 px-4 py-3 
+        ${textColor ? textColor : 'text-white'} 
+        ${
+            isSubmiting ? 'bg-black/50' : bgColor ? bgColor : 'bg-primary-purple'
+        } rounded-xl text-sm font-medium max-md:w-full`}
+        onClick={handleClick}
+    >
+        {leftIcon && <Image src={leftIcon} width={14} height={14} alt='left icon' />}
+        {title}
+        {rightIcon && <Image src={rightIcon} width={14} height={14} alt='right icon' />}
+    </button>
+);
 
 export default Button;
