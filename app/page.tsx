614cad4769b006402723c32bad7f6ce0352405a2
BoyanK95 <boyan_k_95@abv.bg>
2023-07-25 19:32:37 +0300

    feat: create fetchAllProjects in actions, and intial ProjectCard component to displayed all projects in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,45 @@
-const Home = () => {
+import { ProjectInterface } from '@/common.type';
+import ProjectCard from '@/components/ProjectCard';
+import { fetchAllProjects } from '@/lib/actions';
+
+type ProjectSearch = {
+    projectSearch: {
+        edges: { node: ProjectInterface }[];
+        pageInfo: {
+            hasPreviousPage: boolean;
+            hasNextPage: boolean;
+            startCursor: string;
+            endCursor: string;
+        };
+    };
+};
+
+const Home = async () => {
+    const data = (await fetchAllProjects()) as ProjectSearch;
+
+    const projectToDisplay = data?.projectSearch?.edges || [];
+
+    if (projectToDisplay.length === 0) {
+        return (
+            <section className='flexStart flex-col paddings'>
+                TO DO Categories
+
+                <p className='no-result-text text-center'>No projects found, go create some first.</p>
+            </section>
+        );
+    }
+
     return (
-        <section className="flex-start flex-col paddings mb-16">
+        <section className='flex-start flex-col paddings mb-16'>
             <h1>Categories</h1>
-            <h1>Posts</h1>
+            
+            <section className='project-grid'>
+                {projectToDisplay.map(({node} : {node: ProjectInterface}) => <ProjectCard />)}
+            </section>
+
             <h1>LoadMore</h1>
         </section>
     );
 };
 
-export default Home
+export default Home;
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,5 +1,5 @@
 import { ProjectForm } from '@/common.type';
-import { createProjectMutation, createUserMutation, getUserQuery } from '@/graphql';
+import { createProjectMutation, createUserMutation, getUserQuery, projectsQuery } from '@/graphql';
 import { GraphQLClient } from 'graphql-request';
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -77,3 +77,10 @@ export const createNewProject = async (form: ProjectForm, creatorId: string, tok
         return makeGraphQLRequest(createProjectMutation, variables);
     }
 };
+
+
+export const fetchAllProjects =async (category?:string, endcursor?: string) => {
+    client.setHeader('x-api-key', apiKey);
+    
+    return makeGraphQLRequest(projectsQuery, {category, endcursor})
+}
