58a6b2da67a7a249dfa171933c4d1da91975df8e
BoyanK95 <boyan_k_95@abv.bg>
2023-07-25 19:48:20 +0300

    feat: add Link component and Image inside it

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 type Props = {
@@ -10,9 +12,13 @@ type Props = {
 };
 
 const ProjectCard = ({ id, image, title, name, avatarUrl, userId }: Props) => {
-    return <div>
-        
-    </div>;
+    return (
+        <div className='flexCenter flex-col rounded-2xl drop-shadow-card'>
+            <Link href={`/project/${id}`} className='flexCenter group relative w-full h-full'>
+                <Image className='w-full h-full object-cover rounded-2xl' src={image} width={414} height={314} alt='Project Image' />
+            </Link>
+        </div>
+    );
 };
 
 export default ProjectCard;
